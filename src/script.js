a78242fcad5988608d4849aa96ae34e6ad0b1ae2
MadhanM123 <madhangopalvel@gmail.com>
2023-03-15 19:03:50 -0700

    commented calc class

--- src/script.js
+++ src/script.js
@@ -1,51 +1,68 @@
-const numBtns = document.querySelectorAll("[data-num]")
-const opBtns = document.querySelectorAll("[data-op]")
+//Buttons for numbers/operators
+const numBtns = document.querySelectorAll("[data-num]");
+const opBtns = document.querySelectorAll("[data-op]");
 
+//Buttons for equals,delete,clear
 const eqBtn = document.querySelector("[data-eq]")
 const delBtn = document.querySelector("[data-del]")
 const acBtn = document.querySelector("[data-ac]")
 
+//Text elements for previous/current operand
 const prevOpTxt = document.querySelector("[data-prev]")
 const currOpTxt = document.querySelector("[data-curr]")
 
+//CALCULATOR CLASS
 class Calc{
+
+    //Saves previous/current operand text elements and clears
     constructor(prevOpTxt,currOpTxt){
         this.prevOpTxt = prevOpTxt;
         this.currOpTxt = currOpTxt;
         this.clear();
     }
 
+    //Sets operands and operator empty
     clear(){
         this.currOp = "";
         this.prevOp = "";
         this.op = undefined;
     }
 
+    //Deletes latest digit
     delete(){
         this.currOp = this.currOp.toString().slice(0,-1);
     }
 
+    //Appends digit to operand
     appendDigit(num){
         console.log("append");
+        //Only allows 1 decimal
         if(num == "." && this.currOp.includes(num)) return
         this.currOp = this.currOp.toString() + num.toString()
+        console.log(this.currOp);
     }
 
+    //Chooses operator
     chooseOp(op){
         console.log("choosing");
         if(this.currOp == "") return;
+
+        //Chains expressions
         if(this.prevOp != ""){
             this.compute()
         }
+
         this.op = op;
         this.prevOp = this.currOp;
         this.currOp = "";
     }
 
+    //Computes current expression
     compute(){
         let computation;
         const prevNum = parseFloat(this.prevOp);
         const currNum = parseFloat(this.currOp);
+
         if(isNaN(prevNum) || isNaN(currNum)) return;
 
         switch(this.op){
@@ -73,9 +90,12 @@ class Calc{
         this.prevOp = "";
     }
 
+    //Updates display
     update(){
         console.log("updating")
         this.currOpTxt.innerText = this.#displayNum(this.currOp)
+
+        //Moves operand and operator up
         if(this.op != null){
             this.prevOpTxt.innerText = `${this.#displayNum(this.prevOp)} ${this.op}`
         }
@@ -85,12 +105,14 @@ class Calc{
         
     }
 
+    //Handles commas for operands
     #displayNum(num){
         const strNum = num.toString();
         const intDigits = parseFloat(strNum.split(".")[0])
         const decimalDigits = strNum.split(".")[1]
         let intDisplay;
 
+        //Integer digits before decimal
         if(isNaN(intDigits)){
             intDisplay = "";
         }
@@ -98,6 +120,7 @@ class Calc{
             intDisplay = intDigits.toLocaleString("en", {maximumFractionDigits: 0})
         }
 
+        //Digits after decimal
         if(decimalDigits != null){
             return `${intDisplay}.${decimalDigits}`
         }
@@ -105,11 +128,7 @@ class Calc{
             return `${intDisplay}`
         }
     }
-
-
 }
-
-const calculator = new Calc(prevOpTxt,currOpTxt);
 
 numBtns.forEach(btn => {btn.addEventListener("click", () => {
     calculator.appendDigit(btn.innerText)
@@ -136,4 +155,7 @@ delBtn.addEventListener("click", button => {
     calculator.delete()
     calculator.update()
 })
+
+//Single instance of Calc
+const calculator = new Calc(prevOpTxt,currOpTxt);
 
